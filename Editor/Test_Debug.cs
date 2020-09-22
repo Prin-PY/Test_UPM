7912727ec45f3a8027398205dbd1f70cebf35397
PrinceYPeng <PrinceYPeng@163.com>
2020-09-23 00:04:38 +0800

    Add Debug

--- Editor/Test_Debug.cs
+++ Editor/Test_Debug.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
-
-public class Test_Debug : MonoBehaviour
+namespace PrinKindom.Test_UPM
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Test_Debug
     {
-        
+        [MenuItem("Tests/Test_UPM")]
+        private static void TestUPM_Menu()
+        {
+            DebugTest();
+        }
+
+        private static void DebugTest()
+        {
+            Debug.Log("Test UPM");
+        }
+
+        public static void Test()
+        {
+            DebugTest();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
